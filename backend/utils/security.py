f94f085bc2b7260763dca66d6dcaf74ef4eb6d6c
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-12 05:15:44 -0500

    Update security.py

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,1 +1,6 @@
+def secure_headers(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    return response
 
