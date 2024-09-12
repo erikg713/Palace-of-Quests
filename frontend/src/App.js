1674e3260ee367bd71c2ba88f64479b7be0d6057
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-12 05:26:20 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import QuestComponent from './components/QuestComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Palace of Quests</h1>
+        <QuestComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
