c6bbce3bf883af4924613d22d2f95de51796281d
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-03 01:26:13 -0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/piquest')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')  # Separate secret for JWT
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
