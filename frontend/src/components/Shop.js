99493d6ac8e356fa54b1488cccae5cd975692562
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-03 11:01:15 -0600

    Update Shop.js

--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -23,3 +23,36 @@ const Shop = () => {
 };
 
 export default Shop;
+const orderProduct = async (memo, amount, paymentMetadata) => {
+  const paymentData = { amount, memo, metadata: paymentMetadata };
+  const callbacks = {
+    onReadyForServerApproval,
+    onReadyForServerCompletion,
+    onCancel,
+    onError,
+  };
+
+  await window.Pi.createPayment(paymentData, callbacks);
+};
+
+const onReadyForServerApproval = (paymentId) => {
+  console.log("Approving Payment:", paymentId);
+  axios.post("/approve", { paymentId });
+};
+
+const onReadyForServerCompletion = (paymentId, txid) => {
+  console.log("Completing Payment:", paymentId, txid);
+  axios.post("/complete", { paymentId, txid });
+};
+
+const onCancel = (paymentId) => {
+  console.log("Payment Canceled:", paymentId);
+  axios.post("/cancelled_payment", { paymentId });
+};
+
+const onError = (error, payment) => {
+  console.error("Payment Error:", error);
+  if (payment) {
+    console.log("Error Details:", payment);
+  }
+};
