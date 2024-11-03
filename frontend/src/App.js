9c19e88921a6b5cda71cabc4b2ae8e3d2860256f
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-03 01:12:24 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import Profile from './pages/Profile';
+import MobileNav from './components/MobileNav';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/marketplace" component={Marketplace} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/marketplace" component={Marketplace} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+        <MobileNav /> {/* Persistent bottom navigation for mobile */}
+      </div>
     </Router>
   );
 }
