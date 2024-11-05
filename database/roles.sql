423326cd3c4afa6c9b315503b6a3cac3af499d35
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-05 17:12:43 -0600

    Update roles.sql

--- database/roles.sql
+++ database/roles.sql
@@ -8,3 +8,7 @@ GRANT CONNECT ON DATABASE piquest_db TO gameuser;
 
 -- Grant role access only to necessary tables
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO gameuser;
+
+CREATE ROLE gameuser LOGIN PASSWORD 'your_secure_password';
+GRANT CONNECT ON DATABASE palaceofquests_db TO gameuser;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO gameuser;
