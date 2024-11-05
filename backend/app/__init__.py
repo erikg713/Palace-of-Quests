4a797354c96dc2cd330b34f68e8627a088085199
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-05 10:42:24 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,20 +4,17 @@ from flask_jwt_extended import JWTManager
 from .config import Config
 
 db = SQLAlchemy()
-jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-    jwt.init_app(app)
+    JWTManager(app)
 
     with app.app_context():
-        from .routes import auth, marketplace, quests
+        from .routes import auth, payments
         app.register_blueprint(auth.auth_bp, url_prefix='/auth')
-        app.register_blueprint(marketplace.marketplace_bp, url_prefix='/marketplace')
-        app.register_blueprint(quests.quests_bp, url_prefix='/quests')
-
+        app.register_blueprint(payments.payments_bp, url_prefix='/payments')
         db.create_all()
 
     return app
