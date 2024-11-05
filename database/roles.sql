d8f0443da5401eb121685331e685c0741182c57f
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-05 10:46:39 -0600

    Update roles.sql

--- database/roles.sql
+++ database/roles.sql
@@ -1,3 +1,7 @@
+CREATE ROLE gameuser LOGIN PASSWORD 'your_secure_password';
+GRANT CONNECT ON DATABASE piquest_db TO gameuser;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO gameuser;
+
 -- Create gameuser role with login
 CREATE ROLE gameuser LOGIN PASSWORD 'secure_password';
 GRANT CONNECT ON DATABASE piquest_db TO gameuser;
