aa433ac87f30a163df410b15ddffe61e452b836c
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-21 16:47:02 -0600

    Update game_progress.sql

--- database/Tables/game_progress.sql
+++ database/Tables/game_progress.sql
@@ -1,1 +1,6 @@
-a
+CREATE TABLE game_progress (
+    progress_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    level_number INT NOT NULL,
+    completed_at TIMESTAMP DEFAULT NOW()
+);
