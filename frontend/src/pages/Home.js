9c5884594d483be1adaba8731eed16abede5b3fb
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-21 17:32:56 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  text-align: center;
+  padding: 50px;
+`;
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <h1>Welcome to Palace of Quests</h1>
+      <p>Embark on an adventure from Level 1 to 250!</p>
+      <Link to="/dashboard">Start Your Quest</Link>
+    </HomeContainer>
+  );
+};
+
+export default Home;
 import React from 'react';
 
 const Home = () => {
