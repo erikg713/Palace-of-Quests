0a9a4c1a303e3a09a0382e654313719290c1f9f8
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-22 02:27:10 -0600

    Update Shop.js

--- frontend/src/components/Game/Shop.js
+++ frontend/src/components/Game/Shop.js
@@ -1,1 +1,22 @@
-aa
+// components/Game/Shop.js
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Shop</h2>
+      <ul>
+        <li>
+          <span>Upgrade 1</span> - 10 Pi
+          <button>Buy</button>
+        </li>
+        <li>
+          <span>Upgrade 2</span> - 15 Pi
+          <button>Buy</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Shop;
