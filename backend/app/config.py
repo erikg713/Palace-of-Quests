3a70f22f3aeac84d669557c591cc9e0489c244e4
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-28 23:01:51 -0600

    Update config.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,5 @@
-import os
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://username:password@localhost/palace_of_quests")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/palace_of_quests'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PI_NETWORK_API_URL = "https://api.minepi.com"
-    PI_NETWORK_SDK_KEY = os.getenv("PI_NETWORK_SDK_KEY")
+    SECRET_KEY = 'your-secret-key'
+    JWT_SECRET_KEY = 'your-jwt-secret-key'
