6d22d921779dae9f113417ebedbefb7618798823
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 00:27:39 -0600

    Update wsgi.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,3 +5,7 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+import sys
+sys.path.insert(0, '/home/Dev713/PalaceOfQuests')
+
+from app import app as application  # Replace 'app' with your Flask app file name.
