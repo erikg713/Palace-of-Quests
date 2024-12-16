4b6a3c87de26ced3e7e33154297b9e9d8b42bdb1
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-16 00:37:16 -0600

    Update Header.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Header() {
+  return (
+    <header>
+      <h1>Palace of Quests</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/quests">Quests</Link>
+        <Link to="/marketplace">Marketplace</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
