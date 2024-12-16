77878b947bb4aa3b53faf4e20db3833d83e113c2
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-16 00:07:29 -0600

    Update commands.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,4 +1,4 @@
-# Cypress support codes
+
 Cypress.Commands.add('mockAPIs', () => {
   cy.intercept('/api/user/*/profile', { fixture: 'profile.json' }).as('getProfile');
   cy.intercept('/api/user/*/quests', { fixture: 'quests.json' }).as('getQuests');
