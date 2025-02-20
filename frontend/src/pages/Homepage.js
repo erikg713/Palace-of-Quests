f7e1bfb77e38c08bd1de96306f92027c595e5999
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-20 03:26:32 -0600

    Update and rename Home.js to Homepage.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="text-center">
+      <h2>Welcome to Palace of Quests</h2>
+      <p>Embark on an adventure in the metaverse!</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  );
+};
+
+export default HomePage;
