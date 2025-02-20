afbc266d2b8115e1a9a453fa4f3980e53251426d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-20 03:42:31 -0600

    Update config.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/palace_of_quests')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL', 'postgresql://user:password@localhost/palace_of_quests'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
