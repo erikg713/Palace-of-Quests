ace57092de4fde586652cddbb0ab1e2282c782be
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-20 03:27:23 -0600

    Create AboutPage.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="text-center">
+      <h2>About Palace of Quests</h2>
+      <p>This is a Web3-enabled metaverse game built on the Pi Network.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
