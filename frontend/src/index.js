1895ac5a645ba425d8c0aebcf16cd518ba4a2aa6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-24 02:02:47 -0600

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,12 @@ import './styles/global.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <NotificationProvider>
-        <App />
-      </NotificationProvider>
-    </UserProvider>
+    <ErrorBoundary>
+      <UserProvider>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
+      </UserProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
