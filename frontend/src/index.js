9905f0195e3ed26483eab867ec756720f0e50816
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-24 01:58:10 -0600

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for R
 import App from './App';
 import { UserProvider } from './context/UserContext';
 import { NotificationProvider } from './context/NotificationContext';
-
-// If you have global styles
 import './styles/global.css';
-
-// Create a root using React 18's createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render your app wrapped with Context Providers
 root.render(
   <React.StrictMode>
     <UserProvider>
