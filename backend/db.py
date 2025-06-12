31942bf53cb53b9fe2177360516da8cdda2c08d3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-12 04:40:49 +0000

    Update db.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/db.py
+++ backend/db.py
@@ -74,12 +74,3 @@ def get_payment(payment_id: str):
 init_db()
 payments = {}
 
-def store_payment(payment_id, data):
-    payments[payment_id] = data
-
-def update_payment(payment_id, txid):
-    if payment_id in payments:
-        payments[payment_id]['txid'] = txid
-
-def get_payment(payment_id):
-    return payments.get(payment_id)
